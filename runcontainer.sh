413081ae761c1e367c3086bf26b599bd45d0c84c
Giles Richard Greenway <augeas@gmail.com>
2015-04-22 13:39:21 +0100

    modified:   runcontainer.sh

--- runcontainer.sh
+++ runcontainer.sh
@@ -1,1 +1,1 @@
-sudo docker run -i -t -p 6080:6080 -p 80:80 -p 8000:8000 -p 4022:22 --privileged -v /dev/bus/usb:/dev/bus/usb ddk
+docker run -i -t -p 6080:6080 -p 80:80 -p 8000:8000 -p 4022:22 --privileged -v /dev/bus/usb:/dev/bus/usb ddk

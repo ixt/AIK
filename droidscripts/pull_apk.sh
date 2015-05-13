44c94fa5acf570d4c57d6641ac49263a577a1954
Giles Richard Greenway <augeas@gmail.com>
2015-05-13 14:06:57 +0100

    Minor twiddles...  On branch master 	renamed:    applications/androguard-sim.desktop -> applications/androguard-sim.desktopx 	modified:   droidscripts/pull_apk.sh 	modified:   menus/lxde-applications.menu

--- droidscripts/pull_apk.sh
+++ droidscripts/pull_apk.sh
@@ -12,7 +12,7 @@ echo Package: $pkg
 # http://stackoverflow.com/questions/26746853/strange-behaviour-of-adb-pull-in-bash-script
 apk=$(adb shell "su -c 'ls data/app/*.apk '" | grep "$pkg" | tr -d '\r')
 echo .apk file: $apk
-file=$(zenity --file-selection --directory --filename=$HOME --title="Choose a directory to save the .apk file.")
+file=$(zenity --file-selection --directory --filename=$HOME/ --title="Choose a directory to save the .apk file.")
 echo Directory: $file
 cd $file
 adb pull $apk

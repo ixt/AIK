fc3399625bda7cfaf93554c40190d1ddd833f548
Giles Richard Greenway <augeas@gmail.com>
2015-08-13 19:17:02 +0100

    Grab databases if you've got an SD card.  On branch master 	new file:   applications/SQLiteBrowser.desktop 	new file:   applications/androguard-info.desktop 	new file:   applications/ddk-pull-db.desktop 	new file:   droidscripts/do_pull_db.sh 	new file:   droidscripts/pull_db.sh

---
+++ droidscripts/pull_db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Pull the sqlite database from an app.
+# Giles R. Greenway 08/2015
+adb shell "su -c 'mount -o rw,remount /system' "
+pkg=$( adb shell "su -c 'ls data/data' " | grep -P -o "^[a-zA-Z0-9\.]+" | zenity --list --column=Package ) 
+if [ -v "$pkg" ]
+then
+        echo Nothing selected.
+        exit 0
+fi
+echo Package: $pkg
+# http://stackoverflow.com/questions/26746853/strange-behaviour-of-adb-pull-in-bash-script
+db=$(adb shell "su -c 'ls data/data/$pkg/databases ' " | tr -d '\r' ) 
+echo Databases: echo $db
+mkdir -p $HOME/databases/$pkg
+cd $HOME/databases/$pkg
+for f in $db
+do
+    echo $f
+    adb shell "su -c 'cp /data/data/$pkg/databases/$f /sdcard' "
+    adb pull /sdcard/$f $HOME/databases/$pkg
+        adb shell "su -c 'rm /sdcard/$f' "
+done

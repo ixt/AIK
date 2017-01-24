5e8ff16687e3e4884738f9cb5f7970fb8d951eae
root <root@ddk.config>
2017-01-24 14:40:40 +0000

    modified:   runcontainer.sh

--- runcontainer.sh
+++ runcontainer.sh
@@ -1,2 +1,2 @@
-sudo docker run -i -t -p 6080:6080 -p 8080:80 --privileged -v /dev/bus/usb:/dev/bus/usb ddk 
+docker run --rm -i -t -p 8000:8000 -p 6080:6080 -p 8080:80 --privileged -v /dev/bus/usb:/dev/bus/usb ddk 
 
